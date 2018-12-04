0a23aa28c501399ce82510ba5aa87871e96c6b58
1003yd <1003yd@gmail.com>
2018-12-04 21:14:18 +0900

    MainController 수정 (별다른 기능 없음)

--- src/main/java/kr/co/opensise/user/main/web/MainController.java
+++ src/main/java/kr/co/opensise/user/main/web/MainController.java
@@ -1,11 +1,19 @@
 package kr.co.opensise.user.main.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/main")
 public class MainController {
+	
+/*	@Resource(name = "mainService")
+	MainServiceInf mainService;*/
 	
 
 	/**  
@@ -16,9 +24,10 @@ public class MainController {
 	* Method 설명 : 건물분류와 검색명을 가지고 매물 검색   
 	*/
 	@RequestMapping("/main")
-	public String main() {
-		//하라미터를 바탕으로  db에 검색 (파라미터 : 건물분류, 검색명)
+	public String main(Model model, @RequestParam("searchName")String searchName, @RequestParam("building")String building) {
 		
+		//하라미터를 바탕으로  db에 검색 (파라미터 : 건물분류, 검색명)
+		model.addAttribute("search", searchName);
 		return "main";
 	}
 
