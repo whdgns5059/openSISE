a762a53a9c9a04ad80a63d9b52b2f4c20b776093
1003yd <1003yd@gmail.com>
2018-12-04 15:09:35 +0900

    매물리스토 받아오는 VO 생성(BuildingSaleVo) 및 DAO 인터페이스 및  DAO 생성

---
+++ src/main/java/kr/co/opensise/user/main/model/BuildingSaleVo.java
@@ -0,0 +1,19 @@
+package kr.co.opensise.user.main.model;
+
+public class BuildingSaleVo {
+	private String artcl_gu;
+	private String artcl_dong;
+	private String artcl_zip;
+	private String artcl_complx;
+	private String artcl_nm;
+	private String artcl_rd;
+	private String artcl_rd_detail;
+	private String dl_price;
+	private String dl_depos;
+	private String dl_rnt;
+	private String dl_cont_ym;
+	private String dl_cont_d;
+	private String dl_flr;
+	private String dl_excv_area;
+
+}
