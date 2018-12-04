861a00399966d59a4c356895fc220eb82bfdb572
1003yd <1003yd@gmail.com>
2018-12-04 21:13:44 +0900

    BuildingSaleVo getter/setter 추가

--- src/main/java/kr/co/opensise/user/main/model/BuildingSaleVo.java
+++ src/main/java/kr/co/opensise/user/main/model/BuildingSaleVo.java
@@ -15,5 +15,92 @@ public class BuildingSaleVo {
 	private String dl_cont_d;
 	private String dl_flr;
 	private String dl_excv_area;
+	public String getArtcl_gu() {
+		return artcl_gu;
+	}
+	public void setArtcl_gu(String artcl_gu) {
+		this.artcl_gu = artcl_gu;
+	}
+	public String getArtcl_dong() {
+		return artcl_dong;
+	}
+	public void setArtcl_dong(String artcl_dong) {
+		this.artcl_dong = artcl_dong;
+	}
+	public String getArtcl_zip() {
+		return artcl_zip;
+	}
+	public void setArtcl_zip(String artcl_zip) {
+		this.artcl_zip = artcl_zip;
+	}
+	public String getArtcl_complx() {
+		return artcl_complx;
+	}
+	public void setArtcl_complx(String artcl_complx) {
+		this.artcl_complx = artcl_complx;
+	}
+	public String getArtcl_nm() {
+		return artcl_nm;
+	}
+	public void setArtcl_nm(String artcl_nm) {
+		this.artcl_nm = artcl_nm;
+	}
+	public String getArtcl_rd() {
+		return artcl_rd;
+	}
+	public void setArtcl_rd(String artcl_rd) {
+		this.artcl_rd = artcl_rd;
+	}
+	public String getArtcl_rd_detail() {
+		return artcl_rd_detail;
+	}
+	public void setArtcl_rd_detail(String artcl_rd_detail) {
+		this.artcl_rd_detail = artcl_rd_detail;
+	}
+	public String getDl_price() {
+		return dl_price;
+	}
+	public void setDl_price(String dl_price) {
+		this.dl_price = dl_price;
+	}
+	public String getDl_depos() {
+		return dl_depos;
+	}
+	public void setDl_depos(String dl_depos) {
+		this.dl_depos = dl_depos;
+	}
+	public String getDl_rnt() {
+		return dl_rnt;
+	}
+	public void setDl_rnt(String dl_rnt) {
+		this.dl_rnt = dl_rnt;
+	}
+	public String getDl_cont_ym() {
+		return dl_cont_ym;
+	}
+	public void setDl_cont_ym(String dl_cont_ym) {
+		this.dl_cont_ym = dl_cont_ym;
+	}
+	public String getDl_cont_d() {
+		return dl_cont_d;
+	}
+	public void setDl_cont_d(String dl_cont_d) {
+		this.dl_cont_d = dl_cont_d;
+	}
+	public String getDl_flr() {
+		return dl_flr;
+	}
+	public void setDl_flr(String dl_flr) {
+		this.dl_flr = dl_flr;
+	}
+	public String getDl_excv_area() {
+		return dl_excv_area;
+	}
+	public void setDl_excv_area(String dl_excv_area) {
+		this.dl_excv_area = dl_excv_area;
+	}
+	
+	
+	
 
 }
