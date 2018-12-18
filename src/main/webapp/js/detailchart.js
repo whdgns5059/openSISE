8dd4fd310819926d9b17030a89a78fe2079c4ff0
WaterSpray <whdgns5059@gmail.com>
2018-12-18 14:15:29 +0900

    리뷰 modal 글쓰기

--- src/main/webapp/js/detailchart.js
+++ src/main/webapp/js/detailchart.js
@@ -4,163 +4,137 @@ var dl_cont_ymArr = document.getElementsByClassName('avg_dl_cont_ym');
 var priceVal = new Array();
 var contYmVal = new Array();
 
-for(var valIndex = 0 ; valIndex < 30; valIndex++){
-	priceVal[valIndex] = dl_priceArr[valIndex].value;
+for(var valIndex = 0 ; valIndex < dl_priceArr.length; valIndex++){
+	
+	if(dl_priceArr[valIndex].value == 0){
+		
+		if(valIndex == 0){ continue; }
+		
+		dl_priceArr[valIndex].value = dl_priceArr[valIndex - 1].value;
+	}
+	
+	priceVal[valIndex] = parseInt(dl_priceArr[valIndex].value);
 	contYmVal[valIndex] = dl_cont_ymArr[valIndex].value;
 }
 
-console.log(priceVal[0]);
-console.log(contYmVal);
-
 
 var myConfig = {
-	 	type: 'line',
-	 	backgroundColor:'#2C2C39',
-	 	title:{
-	 	  text:'Time Series Data with null values',
-	 	  adjustLayout: true,
-	 	  fontColor:"#E3E3E5",
-	 	  marginTop: 7
-	 	},
-	 	legend:{
-	 	  align: 'center',
-	 	  verticalAlign: 'top',
-	 	  backgroundColor:'none',
-	 	  borderWidth: 0,
-	 	  item:{
-	 	    fontColor:'#E3E3E5',
-	 	    cursor: 'hand'
-	 	  },
-	 	  marker:{
-	 	    type:'circle',
-	 	    borderWidth: 0,
-	 	    cursor: 'hand'
-	 	  }
-	 	},
-	 	plotarea:{
-	 	  margin:'dynamic 70'
-	 	},
-	 	plot:{
-	 	  aspect: 'spline',
-	 	  lineWidth: 2,
-	 	  marker:{
-	 	    borderWidth: 0,
-	 	    size: 5
-	 	  }
-	 	},
-	 	scaleX:{
-	 	  lineColor: '#E3E3E5',
-	 	  zooming: true,
-	 	  zoomTo:[0,15],
-	 	  minValue: 1459468800000,
-	 	  step: 'day',
-	 	  item:{
-	 	    fontColor:'#E3E3E5'
-	 	  },
-	 	  transform:{
-	 	    type: 'date',
-	 	    all: '%D %M %d<br>%h:%i:%s'
-	 	  }
-	 	},
-	 	scaleY:{
-	 	  minorTicks: 1,
-	 	  lineColor: '#E3E3E5',
-	 	  tick:{
-	 	    lineColor: '#E3E3E5'
-	 	  },
-	 	  minorTick:{
-	 	    lineColor: '#E3E3E5'
-	 	  },
-	 	  minorGuide:{
-	 	    visible: true,
-	 	    lineWidth: 1,
-	 	    lineColor: '#E3E3E5',
-	 	    alpha: 0.7,
-	 	    lineStyle: 'dashed'
-	 	  },
-	 	  guide:{
-	 	    lineStyle: 'dashed'
-	 	  },
-	 	  item:{
-	 	    fontColor:'#E3E3E5'
-	 	  }
-	 	},
-	 	tooltip:{
-	 	  borderWidth: 0,
-	 	  borderRadius: 3
-	 	},
-	 	preview:{
-	 	  adjustLayout: true,
-	 	  borderColor:'#E3E3E5',
-	 	  mask:{
-	 	    backgroundColor:'#E3E3E5'
-	 	  }
-	 	},
-	 	crosshairX:{
-	 	  plotLabel:{
-	 	    multiple: true,
-	 	    borderRadius: 3
-	 	  },
-	 	  scaleLabel:{
-	 	    backgroundColor:'#53535e',
-	 	    borderRadius: 3
-	 	  },
-	 	  marker:{
-	 	    size: 7,
-	 	    alpha: 0.5
-	 	  }
-	 	},
-	 	crosshairY:{
-	 	  lineColor:'#E3E3E5',
-	 	  type:'multiple',
-	 	  scaleLabel:{
-	 	    decimals: 2,
-	 	    borderRadius: 3,
-	 	    offsetX: -5,
-	 	    fontColor:"#2C2C39",
-	 	    bold: true
-	 	  }
-	 	},
-	 	shapes:[
-	              {
-	                type:'rectangle',
-	                id:'view_all',
-	                height:'20px',
-	                width:'75px',
-	                borderColor:'#E3E3E5',
-	                borderWidth:1,
-	                borderRadius: 3,
-	                x:'85%',
-	                y:'11%',
-	                backgroundColor:'#53535e',
-	                cursor:'hand',
-	                label:{
-	                  text:'View All',
-	                  fontColor:'#E3E3E5',
-	                  fontSize:12,
-	                  bold:true
-	                }
-	              }
-	            ],
-		series: [
-			{
-				values: priceVal,
-				lineColor:'#E34247',
-				marker:{
-				  backgroundColor:'#E34247'
-				}
-			}
+		"graphset":[
+		    {
+		        "type":"line",
+		        "background-color":"#ECEFF1",
+		        "globals":{
+		          "fontColor":"#212121"
+		        },
+		        "title":{
+		            "text":"실거래 차트",
+		            "adjustLayout": true
+		        },
+		        "plotarea":{
+		            "margin":"dynamic 50 dynamic dynamic",
+		            "backgroundColor":"#fff"
+		        },
+		         "shapes":[
+		              {
+		                "type":"rectangle",
+		                "id":"view_all",
+		                "height":"20px",
+		                "width":"75px",
+		                "border-color":"#777",
+		                "border-width":"1px",
+		                "x":"86%",
+		                "y":"7%",
+		                "background-color":"white",
+		                "alpha":0.6,
+		                "cursor":"hand",
+		                "label":{
+		                  "text":"View All",
+		                  "font-size":12,
+		                  "bold":true
+		                }
+		              }
+		            ],
+		        "scale-x":{
+		            "min-value":1273266300000,
+		            "step":"minute",
+		            "itemsOverlap": true,
+		            "tick":{
+		 
+		            },
+		            "minor-ticks":4,
+		            "minor-tick":{
+		 
+		            },
+		            "transform":{
+		                "type":"date",
+		                "all":"%D, %d %M<br>%h:%i %A"
+		            },
+		            "zooming":true
+		        },
+		        "tooltip":{
+		            "visible":false
+		        },
+		        "scale-y":{
+		            "progression":"demical",
+		            "decimals":0,
+		            "auto-fit":true,
+		            "item":{
+		                "font-weight":"bold"
+		            },
+		            "tick":{
+		            }
+		        },
+		        "crosshair-x":{
+		          "lineColor":"#B71C1C",
+		            "plotLabel":{
+		              "backgroundColor":'#CFD8DC'
+		            },
+		            "scaleLabel":{
+		              "fontColor":"#fff"
+		            },
+		            "marker":{
+		              "backgroundColor":"#263238",
+		              "size": 5,
+		              "type":"triangle",
+		              "angle": 180,
+		              "offsetY": -10,
+		              "offsetX": -1
+		            }
+		        },
+		        "preview":{
+		            "adjustLayout": true,
+		            "live":1
+		        },
+		        "plot":{
+		            "decimals":0,
+		            "line-width":2,
+		            "max-nodes":500,
+		            "lineColor":'#607D8B',
+		            "marker":{
+		                "type":"circle",
+		                "size":3,
+		                "backgroundColor":'#455A64'
+		            },
+		            "tooltip-text":"%v"
+		        },
+		        "series":[
+		            {
+		                "text":"Series 1 Data",
+		                "values": priceVal
+		            }
+		        ]
+		    }
 		]
-	};
-	 
-	zingchart.bind('monthlyAvg', 'shape_click', function(p){
-	  if(p.shapeid == "view_all"){
-	    zingchart.exec(p.id,'viewall');
-	  }
-	})
-	 
-	zingchart.render({ 
-		id: 'monthlyAvg', 
-		data: myConfig, 
-	});
+		};
+		 
+		zingchart.render({ 
+			id : 'monthlyAvg', 
+			data : myConfig, 
+		});
+		 
+		zingchart.shape_click = function(p){
+		  if(p.shapeid == "view_all"){
+		    zingchart.exec(p.id,'viewall');
+		  }
+		}
 	 
