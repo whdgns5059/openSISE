c01e07762227fd30b76e92290e5947eb7a78704c
KimJoooo <nlbo1204@gmail.com>
2018-12-27 22:18:45 +0900

    회원관리 기능추가

--- src/main/java/kr/co/opensise/admin/manage/member/service/MemberServiceInf.java
+++ src/main/java/kr/co/opensise/admin/manage/member/service/MemberServiceInf.java
@@ -1,8 +1,10 @@
 package kr.co.opensise.admin.manage.member.service;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.co.opensise.admin.manage.member.model.ManagementVo;
+import kr.co.opensise.admin.manage.member.model.PageVO;
 
 public interface MemberServiceInf {
 	
@@ -16,4 +18,16 @@ public interface MemberServiceInf {
 	* Method 설명 :  회원 리스트 출력
 	*/
 	public List<ManagementVo> memberList (ManagementVo managementVo);
+	
+	/**  
+	* Method   : memberList 
+	* 작성자 :  김주연
+	* 변경이력 :  
+	* @param 
+	* @return  
+	* Method 설명 :  회원 리스트 출력
+	*/
+	public Map<String, Object> selectUserPageList(PageVO page);
+	
+	
 }
